327d8ce34e7d860e225c2f8a001b4f4687c13d9b
aj98 <abhi.junagade@gmail.com>
2024-08-04 12:12:54 +0530

    Setting up projects utils, assets, configs and constants.

--- my-app/apple15/tailwind.config.js
+++ my-app/apple15/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#2997FF",
+        gray: {
+          DEFAULT: "#86868b",
+          100: "#94928d",
+          200: "#afafaf",
+          300: "#42424570",
+        },
+        zinc: "#101010",
+    },
   },
   plugins: [],
 }
